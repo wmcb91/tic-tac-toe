091c639a41fceb9393a8bf1319009387046f35bc
wmcb91 <wmcb91@gmail.com>
2016-09-08 07:59:22 -0400

    Edit javascript to hide and show elements
    Currently working functions:
      - Signing in hides sign-in-prompt and shows welcome element
      - Create new account link opens sign up modal
      - Submitting sign up modal hides modal and shows welcome element
      - Pressing new game button shows game-board and player-turn
    
    index.js changes:
      - Hide post sign-in elements on page initiation
      - Have game elements appear when actions are taken
      - Add prevent default to sign-in submit and sign-up submit
    
    Other change:
      - Fixed glyphicon-cog for settings bar.

--- README.md
+++ README.md
@@ -2,3 +2,4 @@ Will's Tic-Tac-Toe Game.
 
 Changes to make:
  - Hover over links changes cursor to finger or mouse, not highlight bar.
+ - Make boxes smaller for >1200px
--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -5,3 +5,51 @@
 
 // use require without a reference to ensure a file is bundled
 require('./example');
+
+
+
+// Sign in Event handler shit
+$('#sign-in').submit(function () {
+  event.preventDefault();
+  $('#sign-in-prompt').hide();
+  $('#user-welcome').show();
+});
+
+// Sign up event handler shit
+// Set both prompt links to open modal
+$('#sign-up-link').on('click', function () {
+  $('#signUpModal').modal('show');
+});
+$('#alert-sign-up').on('click', function () {
+  $('#signUpModal').modal('show');
+});
+
+//Submitting sign up closes modal and changes sign-in to welcome
+$('#sign-up').submit(function () {
+  event.preventDefault();
+  $('#sign-in-prompt').hide();
+  $('#sign-up').hide();
+  $('#signUpModal').modal('hide');
+  $('#user-welcome').show();
+});
+
+//*** Need to change later to show #sign-in-warning if not logged in
+// Show #game-board and #player-turn when new game button pressed
+$('#new-game').on('click', function () {
+  $('#game-board').show();
+  $('#player-turn').show();
+});
+
+
+
+  // $('#change-password').show();
+  // $('#log-out').show();
+
+$(document).on('ready', function(){
+  $('#sign-out').hide();
+  $('#change-password').hide();
+  $('#user-welcome').hide();
+  $('#sign-in-warning').hide();
+  $('#player-turn').hide();
+  $('#game-board').hide();
+});
