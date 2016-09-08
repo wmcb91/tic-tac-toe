46064f8a4fe9ec076b6b373b2e4567af538e5438
wmcb91 <wmcb91@gmail.com>
2016-09-08 08:17:02 -0400

    Transfer event handlers to events.js
    - Cut and paste event handler codes to new events.js file
    - Add to readme

--- README.md
+++ README.md
@@ -1,5 +1,19 @@
 Will's Tic-Tac-Toe Game.
 
 Changes to make:
+
+CSS:
  - Hover over links changes cursor to finger or mouse, not highlight bar.
  - Make boxes smaller for >1200px
+
+JS:
+
+
+HTML:
+ - Change change-password to a modal
+ - Add player score
+
+
+
+Down the road ideas:
+ - Change to SCOTUS theme when 9th justice appointed
---
+++ assets/scripts/events.js
@@ -0,0 +1,43 @@
+'use strict';
+
+// Sign in Event handler shit
+$('#sign-in').submit(function () {
+  event.preventDefault();
+  $('#sign-in-prompt').hide();
+  $('#user-welcome').show();
+});
+
+// Sign up event handler shit
+// Set both possible prompt links to open modal
+$('#sign-up-link').on('click', function () {
+  $('#signUpModal').modal('show');
+});
+$('#alert-sign-up').on('click', function () {
+  $('#signUpModal').modal('show');
+});
+
+//Submitting sign up closes modal and changes sign-in to welcome
+$('#sign-up').submit(function () {
+  event.preventDefault();
+  $('#sign-in-prompt').hide();
+  $('#sign-up').hide();
+  $('#signUpModal').modal('hide');
+  $('#user-welcome').show();
+});
+
+//*** Need to change later to show #sign-in-warning if not logged in
+// Show #game-board and #player-turn when new game button pressed
+$('#new-game').on('click', function () {
+  $('#game-board').show();
+  $('#player-turn').show();
+});
+
+//Initially hidden elements
+$(document).on('ready', function(){
+  $('#sign-out').hide();
+  $('#change-password').hide();
+  $('#user-welcome').hide();
+  $('#sign-in-warning').hide();
+  $('#player-turn').hide();
+  $('#game-board').hide();
+});
