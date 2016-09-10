9d6328e1f39310d731b5e539979caa10d85a0b86
wmcb91 <wmcb91@gmail.com>
2016-09-10 18:51:25 -0400

    Add game logic

--- assets/scripts/game/logic.js
+++ assets/scripts/game/logic.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const app = require('../app');
-// const ui = require('./ui');
+const ui = require('./ui');
 
 let currentPlayer = 'X';
 
@@ -9,10 +9,11 @@ let currentPlayer = 'X';
 // on click --> is move allowed?
 // If the cell is occupied (get data from memory or server?)
 // or If the game is over
-// Then the move is not allowed and nothing happens (console log)
+  // Then the move is not allowed and nothing happens (console log)
 // else
-// The move is allowed and a UI function to update the board is called and an
-// API function is called to update the server
+  // The move is allowed
+  // Call UI function to update the board is called and an
+  // Call API function is called to update the server
 
 //allow game move to proceed if allowMove = true
 const allowMove = function (index) {
@@ -27,31 +28,36 @@ const allowMove = function (index) {
     return false;
   }
   else {
+    console.log('You can move');
+    //call UI function to update game board
     return true;
   }
 };
 
 // Did the move win the game?
 // If there are three cells in the same row, column or diagnal with the same value
-  // Then the player with the value in those three cells has won the game
+  // Then the player with the value in those three cells has won the game.
+  // Call UI function to prompt game over modal with Winner message
+  // Call API function to PATCH 'over' to equal true
 // else
   // No one won on this turn, check if there are any empty cells
 
-const gameOver = function () {
-
-  if (('#1' === '#2' && '#1' === '#3' && '#1' !== '') ||
-      ('#1' === '#4' && '#1' === '#7' && '#1' !== '') ||
-      ('#1' === '#5' && '#1' === '#9' && '#1' !== '') ||
-      ('#2' === '#5' && '#2' === '#8' && '#2' !== '') ||
-      ('#3' === '#6' && '#6' === '#9' && '#3' !== '') ||
-      ('#4' === '#5' && '#4' === '#6' && '#4' !== '') ||
-      ('#7' === '#8' && '#7' === '#9' && '#7' !== '') ||
-      ('#7' === '#5' && '#7' === '#3' && '#7' !== '')
+const gameWon = function (index) {
+  let cells = app.user.game.cells;
+  if ((cells[0] === cells[1] && cells[0] === cells[2] && cells[0] !== '') ||
+      (cells[0] === cells[3] && cells[0] === cells[6] && cells[0] !== '') ||
+      (cells[0] === cells[4] && cells[0] === cells[8] && cells[0] !== '') ||
+      (cells[1] === cells[4] && cells[1] === cells[7] && cells[1] !== '') ||
+      (cells[2] === cells[5] && cells[2] === cells[8] && cells[2] !== '') ||
+      (cells[3] === cells[4] && cells[3] === cells[5] && cells[3] !== '') ||
+      (cells[6] === cells[7] && cells[6] === cells[8] && cells[6] !== '') ||
+      (cells[6] === cells[4] && cells[6] === cells[2] && cells[6] !== '')
     )
       {
       //game is over
-      //some player won
-      //display player won
+      // call ui function to display game winner
+      // set game.over === true
+      // call API function to patch 'over' to true
       console.log('Someone won');
       return true;
     }
@@ -61,39 +67,58 @@ const gameOver = function () {
     }
 };
 
+// Are there any empty cells left?
+// If there are no empty cells (a nobody has won)
+  // Then the game is a tie
+  // Call UI Function to prompt game tie modal
+  // Call API function to PATCH server to set 'Over' to true
+// else
+  // Do nothing, move on to see who's turn is next
+
+const gameTied = function (index) {
+  let game = app.user.game;
+  if (game.cells.indexOf('') === -1) {
+    console.log('Game ended in tie');
+    // run UI function
+    // set game.over === true
+    // run API patch function
+    return true;
+  }
+  else {
+    console.log('no tie, keep playing');
+    return false;
+  }
+};
+
 
 // who's turn is it next?
-// the opposite of the currentPlayer
+// the opposite of the currentPlayer if move was allowed
 // changePlayer
 // If currentPlayer is X
-// then change the current player to O
+  // then change the current player to O
+  // Call UI function to display it is O's turn
 // If the currentPlayer is O
-// then change the current player to X
+  // then change the current player to X
+  // Call UI function to display it is X's turn
 
+const changePlayer = function(currentPlayer) {
+  if (allowMove === false) {
+    console.log('no player change, move not allowed');
+    return;
+  }
+  else if (currentPlayer === 'X') {
+    console.log('next up is O');
+    return currentPlayer === 'O';
+  }
+  else if (currentPlayer === 'O') {
+    console.log('next up is X');
+    return currentPlayer === 'X';
+  }
+};
 
+// compile information and send it to UI and API
 
-
-//switch player each turn
-// const changePlayer = function (player) {
-//   player =
-// };
-
-// const gameOver = function (index, value) {
-//
-// };
-
-
-
-
-
-
-
-
-
-
-
-
-
+const executeTurn = function()
 
 
 
