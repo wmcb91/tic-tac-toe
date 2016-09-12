0ac76c52d66773830c325adc569a6e07133f0c8a
wmcb91 <wmcb91@gmail.com>
2016-09-11 21:34:50 -0400

    Switch host
    Switch from HTTP to HTTPS host

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,8 +1,8 @@
 'use strict';
 
 const app = {
-  // host: 'https://aqueous-atoll-85096.herokuapp.com',
-  host: 'http://tic-tac-toe.wdibos.com',
+  host: 'https://aqueous-atoll-85096.herokuapp.com',
+  // host: 'http://tic-tac-toe.wdibos.com',
 };
 
 
