5a76dde13d1912c86e2fbcb4bdbb2c113425ee5f
wmcb91 <wmcb91@gmail.com>
2016-10-28 21:28:41 -0400

    Add to project

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -11,7 +11,16 @@ const gameEvents = require('./game/events');
 $(() => {
   authEvents.addHandlers();
   gameEvents.addHandlers();
-
+  document.body.className += 'loaded';
+  // $( ".title" ).animate({
+  //   width: "70%",
+  //   opacity: 0.4,
+  //   marginLeft: "0.6in",
+  //   fontSize: "3em",
+  //   borderWidth: "10px"
+  // }, 1500 );
+  $('.new-game-btn').hide();
+  setTimeout(function(){$('.new-game-btn').fadeIn(1000);}, 9000);
   $('#pwd-change-msg').hide();
   $('#sign-out').hide();
   $('#user-welcome').hide();
