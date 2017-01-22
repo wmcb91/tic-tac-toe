22535ea6ee4bc1645595ee766606a19d3dc017a6
wmcb91 <wmcb91@gmail.com>
2017-01-22 14:38:35 -0500

    Improve page rendering
    Changes
    - Change game board to display none in html
    - Add button text with jQuery after hidden
    - Hide title before fade in

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -6,20 +6,20 @@ const gameEvents = require('./game/events');
 
 const setBackGround = () => {
   // NOTE: change to lower pixel for vertical ipad
-  if ($(document).width() < 1025) {
-    $(".background").html('');
+  if ($(document).width() < 1025 || $(document).width() === 1366) {
     $(".background").html("<img class='background-vid' src='assets/upside-down-place.png' alt='ud'>");
   }
 };
 
 $(() => {
-  $('.title').hide();
+  setBackGround();
+  $('#new-game-btn-text').html("NEW GAME");
   $('.new-game-btn').hide();
   $('#game-container').hide();
   $('#win-tie-message').hide();
   $('.title').show();
-  gameEvents.addHandlers();
-  setBackGround();
-  $('header').addClass('loaded');
+  $('#play-again-btn-text').html("PLAY AGAIN");
+  setTimeout(function(){$('header').addClass('loaded');}, 1000);
   setTimeout(function(){$('#new-game-btn').show();}, 8000);
+  gameEvents.addHandlers();
 });
